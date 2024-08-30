243cb9b84957df4b0a51d2d37802707f24f09ace
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2024-08-30 22:18:06 +0500

    Update the javascript part of the header section

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let menuBtn = document.querySelector('.header-nav__menu-btn');
 let sidebar = document.querySelector('.sidebar');
 let sidebarInner = document.querySelector('.sidebar-inner');
 let sidebarShade = document.querySelector('.sidebar-shade');
+let headerNav = document.querySelector('.header-nav__list');
 
 
 
@@ -9,8 +10,10 @@ let sidebarShade = document.querySelector('.sidebar-shade');
 menuBtn.addEventListener('click', () => {
     sidebarInner.style.transform = 'translateX(0)';
     sidebarShade.classList.remove('hide');
+    headerNav.style.transform = 'translatex(0)';
 })
 sidebarShade.addEventListener('click', () => {
     sidebarInner.style.transform = 'translateX(-260px)';
     sidebarShade.classList.add('hide');
+    headerNav.style.transform = 'translatex(-200px)';
 })
