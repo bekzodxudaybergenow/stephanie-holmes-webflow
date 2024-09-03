d1f38a351e770c573ea74eeaf137aaf87d5c870e
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2024-09-03 16:42:10 +0500

    Update Header Section

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let menuBtn = document.querySelector('.header-nav__menu-btn');
 let sidebar = document.querySelector('.sidebar');
 let sidebarInner = document.querySelector('.sidebar-inner');
 let sidebarShade = document.querySelector('.sidebar-shade');
-let headerNav = document.querySelector('.header-nav__list');
+let sidebarList = document.querySelector('.sidebar-list');
 let sidebarClosebtn = document.querySelector('.sidebar-closebtn');
 
 
@@ -11,18 +11,18 @@ let sidebarClosebtn = document.querySelector('.sidebar-closebtn');
 menuBtn.addEventListener('click', () => {
     sidebarInner.style.transform = 'translateX(0)';
     sidebarShade.classList.remove('hide');
-    headerNav.style.transform = 'translateX(0)';
+    sidebarList.style.transform = 'translateX(0)';
     sidebarClosebtn.style.transform = 'translateX(0)';
 })
 sidebarShade.addEventListener('click', () => {
     sidebarInner.style.transform = 'translateX(-260px)';
     sidebarShade.classList.add('hide');
-    headerNav.style.transform = 'translatex(-200px)';
+    sidebarList.style.transform = 'translatex(-200px)';
     sidebarClosebtn.style.transform = 'translateX(-250px)';
 })
 sidebarClosebtn.addEventListener('click', () => {
     sidebarInner.style.transform = 'translateX(-260px)';
     sidebarShade.classList.add('hide');
-    headerNav.style.transform = 'translatex(-200px)';
+    sidebarList.style.transform = 'translatex(-200px)';
     sidebarClosebtn.style.transform = 'translateX(-250px)';
 })
